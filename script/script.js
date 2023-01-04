abe34775779f82923d12ca3b79d9a43b55c866dd
Brassy <daniel@MacBook-Pro-Daniil.local>
2023-01-04 13:38:41 +0200

    Added some styles and scripts

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@ $('.header-button').click(function(){
 $('.form').css("transition", "0.5s");
 $('.form').css("margin-top", "0px");
 });
+
+$('.close-form').click(function(){
+   $('.form').css("transition", "0.5s");
+   $('.form').css("margin-top", "-1000px");
+});
