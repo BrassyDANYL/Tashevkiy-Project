d6d6ed555af71956aa26946905523d00d6ef81f7
Daniel Sanko <90936277+BrassyDANYL@users.noreply.github.com>
2023-01-05 18:40:40 +0200

    Delete script directory

--- script/script.js
+++
@@ -1,9 +0,0 @@
-$('.header-button').click(function(){
-$('.form').css("transition", "0.5s");
-$('.form').css("margin-top", "0px");
-});
-
-$('.close-form').click(function(){
-   $('.form').css("transition", "0.5s");
-   $('.form').css("margin-top", "-1000px");
-});
